ac44d9fb9d1b140774719f4424426e89cd296133
robin-douziech <robin.douziech@etu.esisar.grenoble-inp.fr>
2024-10-22 17:44:58 +0200

    fix bug in logs command

--- src/commands_utils.py
+++ src/commands_utils.py
@@ -94,7 +94,7 @@ async def logs_gamebot(ctx: commands.Context, nb_lines: int = 10, *args, **kwarg
     try :
         with open(f"logs/20{year}/{month}/{day}.log", "rt") as f :
             msg = f.read().split("\n")
-            if len(msg) > nb_lines :
+            if len(msg) > int(nb_lines) :
                 msg = msg[-int(nb_lines):]
             txt = "\n".join(msg) + "\n"
             await bot.send(ctx.channel, txt, wrappers=('```', '```'))
